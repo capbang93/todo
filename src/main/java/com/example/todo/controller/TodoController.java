0b473653c75c009d5f0e3dddf922f6e68a65d7b6
Kim <capbang@sookmyung.ac.kr>
2023-03-24 15:16:21 +0900

    백엔드 개발 22. 프로젝트 실습 #2 CRUD

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +43,7 @@ public class TodoController {
 		log.info("Log:dto => entity ok!");
 
 		// entity userId를 임시로 지정한다.
-		entity.setUserId("temporary-userid");
+		entity.setUserId("temporary-user");
 		
 		// service.create 를 통해 repository 에 entity를 저장한다.
 		// 이때 넘어노는 값이 없을 수도 있으므로 List가 아닌 Optional 로 한다.
@@ -74,5 +75,18 @@ public class TodoController {
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
 			return ResponseEntity.badRequest().body(response);
 		}
+	}
+	
+	@GetMapping
+	public ResponseEntity<?>retrieveTodoList(){
+		String temporaryUserId ="temporary-user";
+		List<TodoEntity> entities = service.retrieve(temporaryUserId);
+		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+		
+		// HTTP Status 200 상태로 response 를 전송한다.
+		return ResponseEntity.ok().body(response);
+		
 	}
 }
